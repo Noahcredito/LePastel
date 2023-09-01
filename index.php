e5b90768a6ecfaef3a297f90ca7306bc3051c49b
giovannagliceriodias <giovannagliceriodias@gmail.com>
2023-09-01 10:05:53 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {margin:0;}
+
+.navbar{
+  background-color: #000000;
+  position: fixed;
+  top: 0;
+  width: 200px;
+  height: 100%;
+}
+
+.box{
+  text-align: center;
+  background-color: #FFFFFF;
+  width: 550px;
+  height: 390px;
+  border-radius: 50px;
+  padding: 0.1px;
+  position: absolute;
+  left: 35%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+</head>
+<body style="background-color:#FFE780">
+
+<div class="navbar">
+<img src = "imagens/logo1.png" width="200px" height="200px">
+</div>
+
+<div class="box">
+  <p> SOBRE NÓS </p>
+  <p>O Le Pastel Food Delivery surgiu no ano de 2021, ano de pandemia, época onde muitos estavam sendo dispensados de seus empregos, algo que aconteceu em nosso meio, o desemprego chegou e com isso, a necessidade de se obter uma renda estável também.</p>
+     <p>Já havíamos tido uma experiência em trabalhar nessa área, mas foi em outra cidade e acabou não dando certo, porém devido a todos os ocorridos decidimos abrir essa rede de Delivery.</p>
+     <p>Aqui no Le Pastel Food Delivery podemos garantir a vocês fregueses que não irá faltar sabor no seu pastel, atenção no atendimento, sorteios concorrendo aos nossos deliciosos pastéis, não faltará aquela embalagem embalada com todo amor, nossos funcionários se dedicam ao máximo para que vocês possam receber sempre aquele pastel sequinho e muito saboroso, venham experimentar, faça uma compra conosco todo mês e concorra a brindes incríveis no final dos meses.</p>
+     <p>Obrigada pela atenção.<br>
+     Atenciosamente equipe do Le Pastel Food Delivery!!!</p>	
+</div>
+
+</body>
+</html>
