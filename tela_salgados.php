bee056ab045bb37d009fcb978d4ad56696fe5d30
giovannagliceriodias <giovannagliceriodias@gmail.com>
2023-10-03 12:04:24 -0300

    alteração tela pastéis salgados

--- tela_salgados.php
+++ tela_salgados.php
@@ -4,6 +4,141 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {margin:0;}
+.imagem1{
+  position: absolute;
+  top: 5%;
+  left: 330px;
+}
+.imagem2{
+  position: absolute;
+  top: 5%;
+  left: 630px;
+}
+.imagem3{
+  position: absolute;
+  top: 5%;
+  left: 930px;
+}
+.imagem4{
+  position: absolute;
+  top: 5%;
+  left: 1230px;
+}
+.imagem5{
+  position: absolute;
+  top: 50%;
+  left: 330px;
+}
+.imagem6{
+  position: absolute;
+  top: 50%;
+  left: 630px;
+}
+.imagem7{
+  position: absolute;
+  top: 50%;
+  left: 930px;
+}
+.imagem8{
+  position: absolute;
+  top: 50%;
+  left: 1230px;
+}
+.imagem9{
+  position: absolute;
+  top: 95%;
+  left: 330px;
+}
+.imagem10{
+  position: absolute;
+  top: 95%;
+  left: 630px;
+}
+.imagem11{
+  position: absolute;
+  top: 95%;
+  left: 930px;
+}
+.imagem12{
+  position: absolute;
+  top: 95%;
+  left: 1230px;
+}
+.imagem13{
+  position: absolute;
+  top: 140%;
+  left: 330px;
+}
+.imagem14{
+  position: absolute;
+  top: 140%;
+  left: 630px;
+}
+.imagem15{
+  position: absolute;
+  top: 140%;
+  left: 930px;
+}
+.imagem16{
+  position: absolute;
+  top: 140%;
+  left: 1230px;
+}
+.imagem17{
+  position: absolute;
+  top: 185%;
+  left: 330px;
+}
+.imagem18{
+  position: absolute;
+  top: 230%;
+  left: 330px;
+}
+.imagem19{
+  position: absolute;
+  top: 230%;
+  left: 630px;
+}
+.imagem20{
+  position: absolute;
+  top: 230%;
+  left: 930px;
+}
+.imagem21{
+  position: absolute;
+  top: 230%;
+  left: 1230px;
+}
+.imagem22{
+  position: absolute;
+  top: 275%;
+  left: 330px;
+}
+.imagem23{
+  position: absolute;
+  top: 275%;
+  left: 630px;
+}
+.imagem24{
+  position: absolute;
+  top: 275%;
+  left: 930px;
+}
+.imagem25{
+  position: absolute;
+  top: 275%;
+  left: 1230px;
+}
+.imagem26{
+  position: absolute;
+  top: 320%;
+  left: 330px;
+}
+.imagem27{
+  position: absolute;
+  top: 320%;
+  left: 630px;
+}
 /*.navbar{
   background-color: #000000;
   position: fixed;
@@ -86,5 +221,114 @@ label span:nth-child(3){
     <img src = "imagens/engrenagem.png" width="40px" height="40px">
   </div>
 </div-->
+
+  <div class="imagem1">
+    <img src = "imagens/vento.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem2">
+    <img src = "imagens/lombo.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem3">
+    <img src = "imagens/calabresa.png" width="250px" height="250px">
+  </div>
+  
+  <div class="imagem4">
+    <img src = "imagens/frango.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem5">
+    <img src = "imagens/carne.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem6">
+    <img src = "imagens/carne_com_azeitona.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem7">
+    <img src = "imagens/carne_com_queijo.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem8">
+    <img src = "imagens/misto.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem9">
+    <img src = "imagens/pizza.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem10">
+    <img src = "imagens/frango_a_moda.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem11">
+    <img src = "imagens/file_ao_alho.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem12">
+    <img src = "imagens/dog_misto.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem13">
+    <img src = "imagens/calabresa_especial.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem14">
+    <img src = "imagens/paulista.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem15">
+    <img src = "imagens/a_portuguesa.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem16">
+    <img src = "imagens/misto_especial.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem17">
+    <img src = "imagens/frango_especial.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem18">
+    <img src = "imagens/frango_queijo_azeitona_catupiry.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem19">
+    <img src = "imagens/dog_especial.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem20">
+    <img src = "imagens/file_especial.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem21">
+    <img src = "imagens/file_ao_alho_catupiry.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem22">
+    <h4>VEGETARIANOS</h4>
+    <img src = "imagens/brocolis.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem23">
+    <img src = "imagens/queijo.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem24">
+    <img src = "imagens/palmito_cremoso.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem25">
+    <img src = "imagens/brocolis_com_queijo.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem26">
+    <img src = "imagens/vegetariano.png" width="250px" height="250px">
+  </div>
+
+  <div class="imagem27">
+    <img src = "imagens/4_queijos.png" width="250px" height="250px">
+  </div><br><br><br><br><br><br><br>
 </body>
 </html>
