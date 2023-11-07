24cfd8633090eb9eb7bcf74043ba254223948056
Noahcredito <noah0goes@gmail.com>
2023-11-07 16:23:16 -0300

    Update bd_lepastel

--- banco/bd_LePastel.sql
+++ banco/bd_LePastel.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS BD_LEPASTEL;
+
 CREATE DATABASE BD_LEPASTEL;
 
 USE BD_LEPASTEL;
@@ -17,26 +19,17 @@ CREATE TABLE tb_cliente
  senha_cli VARCHAR(15),
  email_cli VARCHAR(40),
  bairro_cli VARCHAR(20),
- complemento_cli VARCHAR(50),
  rua_cli VARCHAR(40),
  numero_cli VARCHAR(4),
- telefone_cli VARCHAR(15),
- cep_cli VARCHAR(10),
- cidade_cli VARCHAR(50)
+ telefone_cli VARCHAR(15)
 );
 
 CREATE TABLE tb_sorteio
 (
- id_sort INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ id_sor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  cod_cli INT NOT NULL,
  cod_adm INT NOT NULL,
- data_sort DATE,
+ data_sor DATE,
  FOREIGN KEY(cod_cli) REFERENCES tb_cliente(id_cli),
  FOREIGN KEY(cod_adm) REFERENCES tb_adm(id_adm)
-);
-
-CREATE TABLE tb_cli_sort
-(
-cod_cli INT NOT NULL,
-FOREIGN KEY(cod_cli) REFERENCES tb_cliente(id_cli)
 );
