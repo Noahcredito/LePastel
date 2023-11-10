d59173907a15653e8ce1a8fba311307eeaea6198
Noahcredito <noah0goes@gmail.com>
2023-11-10 16:33:06 -0300

    Começo da atualização do alterar cliente

--- cliente/altera_cli.php
+++ cliente/altera_cli.php
@@ -1,45 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <!-- Meta tags Obrigatórias -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Alteração de Cliente</title>
-  </head>
-  <body>
-   <div class="container">
+<head>
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Alteração de Cliente</title>
+</head>
+
+<body>
+  <div class="container">
     <?php
 
     require "../conexao.php";
-    
-      $id=$_POST["id_cli"];
-	    $nome=$_POST["nome_cli"];
-      $email=$_POST["email_cli"];
-      $senha=$_POST["senha_cli"];
-      $bairro=$_POST["bairro_cli"];  
-      $cep=$_POST["cep_cli"];
-      $cidade=$_POST["cidade_cli"];      
-      $rua=$_POST["rua_cli"];
-      $numero=$_POST["numero_cli"];
-      $complemento=$_POST["complemento_cli"];
-      $telefone=$_POST["telefone_cli"];
-	  
-	$comandoSql="update tb_cliente set 
-	nome_cli='$nome', email_cli='$email', senha_cli='$senha', bairro_cli='$bairro', cep_cli='$cep', cidade_cli='$cidade',
-    rua_cli='$rua', numero_cli='$numero', complemento_cli='$complemento', telefone_cli='$telefone' where id_cli='$id'";
-	
-	$resultado=mysqli_query($con,$comandoSql);
-	
-     if($resultado==true)
-		  echo "Alterado com sucesso";
-	 else
-		 echo "Erro na alteração";
-	 
-	
 
-  ?>
-  
+    $id = $_POST["id_cli"];
+    $nome = $_POST["nome_cli"];
+    $email = $_POST["email_cli"];
+    $senha = $_POST["senha_cli"];
+    $bairro = $_POST["bairro_cli"];
+    $cep = $_POST["cep_cli"];
+    $cidade = $_POST["cidade_cli"];
+    $rua = $_POST["rua_cli"];
+    $numero = $_POST["numero_cli"];
+    $complemento = $_POST["complemento_cli"];
+    $telefone = $_POST["telefone_cli"];
+
+    $comandoSql = "update tb_cliente set 
+    nome_cli='$nome', email_cli='$email', senha_cli='$senha', bairro_cli='$bairro', cep_cli='$cep', cidade_cli='$cidade',
+    rua_cli='$rua', numero_cli='$numero', complemento_cli='$complemento', telefone_cli='$telefone' where id_cli='$id'";
+
+    $resultado = mysqli_query($con, $comandoSql);
+
+    if ($resultado == true)
+      echo "Alterado com sucesso";
+    else
+      echo "Erro na alteração";
+
+
+
+    ?>
+
   </div>
-    </body>
+</body>
+
 </html>
--- cliente/frm_altera_cli.php
+++ cliente/frm_altera_cli.php
@@ -1,106 +1,99 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <!-- Meta tags Obrigatórias -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Alteração de Cliente</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Alteração de Cliente</title>
+</head>
+
+<body>
   <div class="container">
     <h3> Alteração de Cliente </h3>
-   <?php
+    <?php
 
-     /*1- realizando a conexao com o banco de dados (local, usuario,senha,nomeBanco)*/
-     //$con=mysqli_connect("localhost", "root", "", "bd_LePastel");
-     require "../conexao.php";
-    
-    /*2- pegando o valor vindo da url */
-	  $id=$_GET["id_cli"];
-    
-    /*3- criando o comando sql para listar os dados do cliente selecionado */
-	  $comandoSql="select * from tb_cliente where id_cli='$id'";
-	
-	/*4- executando o comando sql */
-	$resultado=mysqli_query($con, $comandoSql);
-	
-	/*5- pegando os dados da consulta criada e armazenando em variaveis */
-  $dados= mysqli_fetch_assoc($resultado);
+    require "../conexao.php";
 
-  $nome=$_POST["nome_cli"];
-  $email=$_POST["email_cli"];
-  $senha=$_POST["senha_cli"];
-  $cep=$_POST["cep_cli"];
-  $cidade=$_POST["cidade_cli"];
-  $bairro=$_POST["bairro_cli"];        
-  $rua=$_POST["rua_cli"];
-  $numero=$_POST["numero_cli"];
-  $complemento=$_POST["complemento_cli"];
-  $telefone=$_POST["telefone_cli"];
+    $id = $_SESSION["user_id"];
 
-  ?>
-  <!--preenchendo o formulario com dados da consulta -->
-  <form action="altera_cli.php" method="POST">
-  <div class="form-group">
-      <label for="id"> Id </label>
-      <input readonly type="text"  id="id" class="form-control" value="<?php echo $id?>">
-	 	 </div>
+    $comandoSql = "select * from tb_cliente where id_cli='$id'";
 
-     <div class="form-group">
-      <label for="nome"> Nome </label>
-      <input type="text"  id="nome" class="form-control" value="<?php echo $nome?>">
-	 	 </div>
+    $resultado = mysqli_query($con, $comandoSql);
 
-     <div class="form-group">
-	   <label for="email"> Email </label>
-       <input type="email" id="email" class="form-control" value="<?php echo $email?>">
-	  </div> 
+    $dados = mysqli_fetch_assoc($resultado);
+
+    $nome = $_POST["nome_cli"];
+    $email = $_POST["email_cli"];
+    $senha = $_POST["senha_cli"];
+    $cep = $_POST["cep_cli"];
+    $cidade = $_POST["cidade_cli"];
+    $bairro = $_POST["bairro_cli"];
+    $rua = $_POST["rua_cli"];
+    $numero = $_POST["numero_cli"];
+    $complemento = $_POST["complemento_cli"];
+    $telefone = $_POST["telefone_cli"];
+
+    ?>
+
+    <form action="altera_cli.php" method="POST">
 
       <div class="form-group">
-	   <label for="senha"> Senha </label>
-       <input type="senha" id="senha" class="form-control" value="<?php echo $senha?>">
-	  </div> 
+        <label for="nome"> Nome </label>
+        <input type="text" id="nome" class="form-control" value="<?php echo $nome ?>">
+      </div>
 
       <div class="form-group">
-	   <label for="bairro"> Bairro </label>
-       <input type="bairro" id="bairro" class="form-control" value="<?php echo $bairro?>">
-	  </div> 
-
-    <div class="form-group">
-	   <label for="cep"> CEP </label>
-       <input type="cep" id="cep" class="form-control" value="<?php echo $bairro?>">
-	  </div> 
-
-    <div class="form-group">
-	   <label for="cidade"> Cidade </label>
-       <input type="cidade" id="cidade" class="form-control" value="<?php echo $bairro?>">
-	  </div> 
+        <label for="email"> Email </label>
+        <input type="email" id="email" class="form-control" value="<?php echo $email ?>">
+      </div>
 
       <div class="form-group">
-	   <label for="rua"> Rua </label>
-       <input type="rua" id="rua" class="form-control" value="<?php echo $rua?>">
-	  </div> 
+        <label for="senha"> Senha </label>
+        <input type="senha" id="senha" class="form-control" value="<?php echo $senha ?>">
+      </div>
 
       <div class="form-group">
-	   <label for="numero"> Numero </label>
-       <input type="numero" id="numero" class="form-control" value="<?php echo $numero?>">
-	  </div> 
-
-    <div class="form-group">
-	   <label for="complemento"> Complemento </label>
-       <input type="complemento" id="complemento" class="form-control" value="<?php echo $numero?>">
-	  </div> 
+        <label for="cep"> CEP </label>
+        <input type="cep" id="cep" class="form-control" value="<?php echo $bairro ?>">
+      </div>
 
       <div class="form-group">
-	   <label for="telefone"> Telefone </label>
-       <input type="telefone" id="telefone" class="form-control" value="<?php echo $telefone?>">
-	  </div> 
-	     <input type="submit" value="Alterar" class="btn btn-outline-secondary">
+        <label for="cidade"> Cidade </label>
+        <input type="cidade" id="cidade" class="form-control" value="<?php echo $bairro ?>">
+      </div>
 
-   </form>
-  
-  
+      <div class="form-group">
+        <label for="bairro"> Bairro </label>
+        <input type="bairro" id="bairro" class="form-control" value="<?php echo $bairro ?>">
+      </div>
+
+      <div class="form-group">
+        <label for="rua"> Rua </label>
+        <input type="rua" id="rua" class="form-control" value="<?php echo $rua ?>">
+      </div>
+
+      <div class="form-group">
+        <label for="numero"> Numero </label>
+        <input type="numero" id="numero" class="form-control" value="<?php echo $numero ?>">
+      </div>
+
+      <div class="form-group">
+        <label for="complemento"> Complemento </label>
+        <input type="complemento" id="complemento" class="form-control" value="<?php echo $numero ?>">
+      </div>
+
+      <div class="form-group">
+        <label for="telefone"> Telefone </label>
+        <input type="telefone" id="telefone" class="form-control" value="<?php echo $telefone ?>">
+      </div>
+
+      <input type="submit" value="Alterar" class="btn">
+
+    </form>
+
+
   </div>
- </body>
+</body>
+
 </html>
