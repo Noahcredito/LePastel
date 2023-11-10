ef73c316eaea8cfcd844bdda980380b88148b83e
giovannagliceriodias <giovannagliceriodias@gmail.com>
2023-11-10 11:32:54 -0300

    progresso positivo

--- index.php
+++ index.php
@@ -37,146 +37,12 @@
   transform: translateY(-50%);
   left: 65%;
 }
-
-@media screen and (min-width: 320px) and (max-width: 480px){
-  .box{
-    font-size: 16.06px;
-    text-align: center;
-    background-color: #FFFFFF;
-    width: 460px;
-    height: 460px;
-    border-radius: 50px;
-    padding: 0.1px;
-    position: absolute;
-    top: 70%;
-    transform: translateY(-50%);
-    left: 37%;
-    font-family: Alice;
-  }
-
-  .imagem1{
-    padding: 0.1px;
-    position: absolute;
-    top: 150%;
-    transform: translateY(-50%);
-    left: 25%;
-  }
-
-  .imagem2{
-    padding: 0.1px;
-    position: absolute;
-    top: 150%;
-    transform: translateY(-50%);
-    left: 30%;
-  }
-}
-
-@media screen and (min-width: 769px) and (max-width: 1024px){
-  .box{
-    font-size: 10px;
-    text-align: center;
-    background-color: #FFFFFF;
-    width: 200px;
-    height: 150px;
-    border-radius: 50px;
-    padding: 0.1px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 20%;
-    font-family: Alice;
-  }
-
-  .imagem1{
-    padding: 0.1px;
-    position: absolute;
-    top: 34.5%;
-    transform: translateY(-50%);
-    left: 65%;
-  }
-
-  .imagem2{
-    padding: 0.1px;
-    position: absolute;
-    top: 75%;
-    transform: translateY(-50%);
-    left: 75%;
-  }
-}
-
-@media screen and (min-width: 1025px) and (max-width: 1200px){
-  .box{
-    font-size: 10px;
-    text-align: center;
-    background-color: #FFFFFF;
-    width: 200px;
-    height: 150px;
-    border-radius: 50px;
-    padding: 0.1px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 20%;
-    font-family: Alice;
-  }
-
-  .imagem1{
-    padding: 0.1px;
-    position: absolute;
-    top: 34.5%;
-    transform: translateY(-50%);
-    left: 65%;
-  }
-
-  .imagem2{
-    padding: 0.1px;
-    position: absolute;
-    top: 75%;
-    transform: translateY(-50%);
-    left: 75%;
-  }
-}
-
-@media screen and (min-width: 1920px){
-  .box{
-    font-size: 23.7px;
-    text-align: center;
-    background-color: #FFFFFF;
-    width: 650px;
-    height: 650px;
-    border-radius: 50px;
-    padding: 0.1px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 20%;
-    font-family: Alice;
-  }
-
-  .imagem1{
-    padding: 0.1px;
-    position: absolute;
-    top: 37%;
-    transform: translateY(-50%);
-    left: 59%;
-  }
-
-  .imagem2{
-    padding: 0.1px;
-    position: absolute;
-    top: 63%;
-    transform: translateY(-50%);
-    left: 66%;
-  }
-}
 </style>
 </head>
 <body>
 <?php
 
 require 'barra_lateral.php'?>
-<div class="container">
-<div class="item">
 <div class="box">
     <p> SOBRE NÓS </p>
     <p>A Le Pastel nasceu do sonho da sua fundadora de empreender e compartilhar suas delícias com todos que amam uma boa comida. No ano de 2021 o projeto foi tirado do papel e finalmente o sonho teve início.</p>
@@ -193,8 +59,6 @@ require 'barra_lateral.php'?>
   <div class="imagem2">
     <img src="imagens/foto2_inicio.jpeg" width="550px" height="320px">
   </div>
-</div>
-</div>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
