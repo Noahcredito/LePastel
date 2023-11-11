ab915c593193e2e601e1c9e1cfa7b7c2611a0723
Noahcredito <noah0goes@gmail.com>
2023-11-10 22:21:47 -0300

    Retirada de comentários

--- js/script.js
+++ js/script.js
@@ -1,51 +1,40 @@
 $(document).ready(function () {
-	$('#cep').blur(//quando o campo de cep perder o foco
+	$('#cep').blur(
 		function(){
-			if($('#cep').val().length==0) {//se nao tiver nada digitado 
-			   /*$('#alerta').modal('show');*/ //exibir a modal de nada digitado
+			if($('#cep').val().length==0) {
 			   let xhr = new XMLHttpRequest();
   				xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      // A função exibirAlerta() foi carregada do outro documento e agora pode ser chamada.
-      /*window.alerta();*/
+    
     }
   };
   xhr.open("GET", "../cliente/frm_cadastra_cli.php", true);
   xhr.send();
 			   $("#cep").focus();
 			}
-			else{//se tiver um cep digitado
+			else{
 				
-				let dados;//declaracao da variavel dados
-				dados={cep:$('#cep').val()};//pegando o valor do que foi digitado e guardando em dados
-				
-                //chamando o arquivo pesquisaCep.php, passando o que foi armazenando em dados 
-				//aguardando o retorno atraves das function que retornará as informacoes do cep 
+				let dados;
+				dados={cep:$('#cep').val()};
 				            				    
                 $.post('pesquisaCep.php',dados,function(retornaCep){
-					 //a variavel resultado recebe o que veio do pesquisaCep.php, divide 
-					 //a string em pedacos e coloca em um array usando um parametro
-					 //(neste caso foi a virgula)   
+  
 					   resultado = retornaCep.split(",");
 					
-						cidade=resultado[0]; //1º elemento do array
+						cidade=resultado[0];
 						rua=resultado[1];
 						bairro=resultado[2];
 						complemento=resultado[3];
 
 						
-						$('#cidade').val(cidade); //mandando exibir o nome da cidade no campo inputCidade do formulario
+						$('#cidade').val(cidade);
 						$('#rua').val(rua);
 						$('#bairro').val(bairro);
 						$('#complemento').val(complemento);
 
-					});//fim do $.post
-			}//fim do else*/
+					});
+			}
  
-   });//fim da function()
-
-//dá o focus na caixa de textos do cep depois de exibida a mensagem de erro, ou seja,
-//o cep nao foi digitado. 
-
+   });
   	
-});//fim da (document).ready
+});
